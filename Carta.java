89cd582da0ff1843f8628560fa9f32d4ef362a45
RaulEncinas <raulencinas280@gmail.com>
2014-04-04 17:57:07 +0200

    Create Carta.java

---
+++ Carta.java
@@ -0,0 +1,16 @@
+package principal;
+
+public class Carta {
+	 private String cara;
+	 private String palo;
+	 
+	 public Carta( String caraCarta, String paloCarta){
+		 cara = caraCarta;
+		 palo = paloCarta;
+
+	 }
+
+	 public String toString(){
+		 return cara + " de " + palo;
+	 }
+}
